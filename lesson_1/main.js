e6741fc85e4823e6d912eca863e482b0f359fb30
WongoBongo <wingeddeath62@gmail.com>
2020-04-14 16:05:49 +0500

    lesson_1

---
+++ lesson_1/main.js
@@ -0,0 +1,17 @@
+/* console.log(3);
+alert('1');
+console.log(2); */
+console.log('myVar', myVar);
+var myVar = 10;
+let myAge = 22;
+const pi = 3.14;
+console.log('myVar', myVar);
+
+let money = 20;
+var income = 'Текст';
+const addExpenses = 10;
+let deposit = 40;
+var mission = 'Пройти курс';
+const period =  40;
+alert('Начало моего пути в js');
+console.log('Хочу пройти курс по js');
